842aa6e68be81e0eb6630818e3db862bb482cc9b
Naomi Ahmed <yretenai@gmail.com>
2022-12-31 14:25:37 +0000

    this fixes it

--- src/components/Orbit.svelte
+++ src/components/Orbit.svelte
@@ -22,7 +22,7 @@
     <a {href} target="_blank" rel="noreferrer">
         <div class="orbit" style:animation-duration="{distance/100*25}s" style:width="{distance + offset}pt" style:height="{distance + offset}pt" style:left="-{(distance + offset)/2}pt" style:top="-{(distance + offset)/2}pt">
             <svg class="border" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" preserveAspectRatio="none">
-                <circle cx="100" cy="100" r="95" stroke="currentColor" stroke-width="0.3" stroke-dasharray="2" fill="none"/>
+                <circle cx="100" cy="100" r="95" stroke="currentColor" stroke-width="0.3" stroke-dasharray="2 4.5" fill="none"/>
                 <circle cx="100" cy="100" r="100" fill="transparent"/>
             </svg>
             <div class="dot" style:animation-duration="{distance/100*25}s"
